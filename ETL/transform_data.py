44356f81df568a301e287ba234a03d1664d0a3e8
Norbert <norbee1826@gmail.com>
2023-10-10 22:21:04 +0200

    Added data transformation related file.

---
+++ ETL/transform_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import duckdb as ddb
+
+def transfrom_data(input):
+    normalized_data = pd.json_normalize(input)
+    return normalized_data
+
+def transform_drivers_data(input):
+    normalized_data = pd.json_normalize(input)
+    return normalized_data
+
+def transform_drivers_data(input):
+    normalized_data = pd.json_normalize(input)
+    return normalized_data
+
+def transform_drivers_data(input):
+    normalized_data = pd.json_normalize(input)
+    return normalized_data
