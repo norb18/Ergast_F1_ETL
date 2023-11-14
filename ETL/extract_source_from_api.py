52c445cab396e432b0741321fd522d3148abea75
Norbert <norbee1826@gmail.com>
2023-11-14 20:08:21 +0100

    Changed extract process.

--- ETL/extract_source_from_api.py
+++ ETL/extract_source_from_api.py
@@ -9,28 +9,28 @@ def get_response(url: str):
     response = requests.api.get(f'https://ergast.com/api/f1/{url}.json')
     return response
 
-def handle_drivers(api_endpoint):
+def handle_drivers(api_endpoint) -> pd.DataFrame:
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
     response = requests.api.get(url)
     json_drivers = response.json()
     drivers_df = pd.json_normalize(json_drivers['MRData']['DriverTable']['Drivers'])
     return drivers_df
 
-def handle_standingList(api_endpoint):
+def handle_standingList(api_endpoint) -> pd.DataFrame:
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
     response = requests.api.get(url)
     json_standinglist = response.json()
     standings_df = pd.json_normalize(json_standinglist['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'])
     return standings_df
 
-def handle_races(api_endpoint):
+def handle_races(api_endpoint) -> pd.DataFrame:
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
     response = requests.api.get(url)
     json_races = response.json()
     races_df = pd.json_normalize(json_races['MRData']['RaceTable']['Races'])
     return races_df
 
-def handle_circuits(api_endpoint):
+def handle_circuits(api_endpoint) -> pd.DataFrame:
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
     response = requests.api.get(url)
     json_circuits = response.json()
